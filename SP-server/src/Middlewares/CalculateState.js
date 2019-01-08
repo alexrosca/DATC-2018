915fd5a906267d35663868adf61f252e01901a1a
Agota Szabo <agota95@gmail.com>
2019-01-08 18:19:17 +0200

    New: wrote CalculateState middleware.

--- SP-server/src/Middlewares/CalculateState.js
+++ SP-server/src/Middlewares/CalculateState.js
@@ -1,8 +1,29 @@
 module.exports = { 
+    /**
+     * if sensorValue === 1 -> occupied
+     * if sensorValue === 0 -> free
+     * if none of these above, than its unknown
+     */
     calculateState: (req,res,next) => {
-        /**
-         * Calculate the new state, change it in req.body.sensorValue after calculation
-         */
+        let futureValue
+
+        // decider
+        switch(req.body.sensorValue){
+            case 0: {
+                futureValue = "free"
+                break
+            }
+            case 1: {
+                futureValue = "occupied"
+                break
+            }
+            default: {
+                futureValue = "unknown"
+            }
+        }
+
+        // equaling the sensorValue to the calculated value
+        req.body.sensorValue = futureValue
 
         return next()
     }
